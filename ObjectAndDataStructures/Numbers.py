083fbb25af10a9c522830f2f9b74b100c0ef4eab
prog <grigorij9026@gmail.com>
2016-12-27 00:11:17 +0100

    komment pontosítás

--- ObjectAndDataStructures/Numbers.py
+++ ObjectAndDataStructures/Numbers.py
@@ -36,7 +36,7 @@ print(5 ** (1/n))       # 1.3076604860118306 ( az 5 n-edik gyöke )
 - nevek (változók) nem kezdődhetnek számmal
 - nem lehet space a nevekben ('_' karakter használata javasolt helyette)
 - nem használhatóak ezek a karakterek: :'",<>/?|\()!@#$%^&*~-+
-- a bevett gyakorlat szerint a neveket kisbetűvel írjuk
+- a konvenciók szerint a neveket kisbetűvel írjuk
 """
 
 my_income = 100
@@ -44,3 +44,5 @@ tax_rate = .1
 my_taxes = my_income * tax_rate
 
 print(my_taxes.__trunc__())     # 10  –  trunc nélkül 10.0 lett volna
+
+
