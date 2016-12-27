159eb50dec476bfcc102e77d9ac8fb4493e87363
prog <grigorij9026@gmail.com>
2016-12-27 17:40:06 +0100

    Összefűzött logikai operátorok használata

---
+++ Comparison/ChainedComparison.py
@@ -0,0 +1,10 @@
+# lehetséges a logikai operátorok összefűzése a következőképp:
+
+print(1 < 2 < 3)                            # True
+
+# ez szöveges operátorral a következő:
+
+print(1 < 2 and 2 < 3)                      # True
+
+
+print(1 < 3 > 2)                            # True  -  ez pedig gyakorlatilag egyenlő ezzel: 1 < 3 and 3 > 2
